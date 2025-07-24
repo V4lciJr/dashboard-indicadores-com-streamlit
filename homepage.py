bea42eb1e99d74e7def33098a231d382ea87201a
Valci Júnior <valcijunior19@hotmail.com>
2025-07-23 22:08:21 -0300

    criacao da pagina homepage

--- homepage.py
+++ homepage.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
-st.title("Homepage")
+
+coluna_esquerda, coluna_direita = st.columns([1, 2])
+
+coluna_esquerda.title("Homepage")
+coluna_esquerda.write("#### Bem vindo, Valci")
+
+botao_dashboards = coluna_esquerda.button("Dashboards Projetos")
+botao_indicadores = coluna_esquerda.button("Principais Indicadores")
+
+
+container = coluna_direita.container(border=True)
+container.image("imagens/home.png")
+
+if botao_dashboards:
+    st.switch_page("dashboard.py")
+elif botao_indicadores:
+    st.switch_page("indicadores.py")
